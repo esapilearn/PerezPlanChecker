eee46d1d105391c668a54d2262bed11c15981493
MDP <mario.perez@y7mail.com>
2021-02-24 19:52:38 -0800

    added unit test

---
+++ PerezPlanCheckerTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void IsCTTooOldTest()
+        {
+           //arrange
+            var daysOld = 61;
+
+            //act
+            var actual = new CTDateConstraint().IsCTTooOld(daysOld).ResultType;
+
+            //assert
+            var expected = ResultType.ACTION_LEVEL_3;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
