06324d0e3a6fff7d6aaacfa0f5c0abadac6b03ce
MDP <mario.perez@y7mail.com>
2021-02-24 20:10:31 -0800

    final deployment

--- PerezPlanChecker/App.xaml.cs
+++ PerezPlanChecker/App.xaml.cs
@@ -23,7 +23,7 @@ namespace ESAPX_StarterUI
         private IESAPIService _esapiServ;
 
         //Disable if you don't want patient selection
-        public bool IsPatientSelectionEnabled { get; } = true;
+        public bool IsPatientSelectionEnabled { get; } = false; //changed from true
 
         private string[] _args;
 
